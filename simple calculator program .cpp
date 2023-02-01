5dc17468b2eed33c630c6fbf0c2b59fbdde73d8e
sivaprakashrmalai <103348485+sivaprakashrmalai@users.noreply.github.com>
2023-02-01 14:21:44 +0530

    Create simple calculator program .cpp

---
+++ simple calculator program .cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	// various type of calculations :
+	int a,b;
+	cout<<"Enter the numbers :"<<endl;
+	cin>>a>>b;
+	int c=a+b;     //the addition two numbers :
+	cout<<"the adition of two numbers :"<<c<<endl;  
+	int d=a-b;   //the addition two numbers :
+	cout<<"the subtraction of two numbers :"<<d<<endl; 
+	int e=a*b;   //the addition two numbers :
+	cout<<"the multiplication of two numbbers :"<<e<<endl;
+	int f=a/b;   //the addition two numbers :
+	cout<<"the division of two numbers :"<<f<<endl;
+	int g=sqrt(a);   //the addition two numbers :
+	cout<<"the square root of one value :"<<g<<endl;
+	int h=sqrt(b);  //the addition two numbers : 
+	cout<<"the square root of another value :"<<h<<endl;
+	int i=pow(a,b);  //the power of one value :
+	cout<<"the power of value :"<<i<<endl;
+	int j=pow(b,a);  //the power of one value :
+	cout<<"the power of another value :"<<j<<endl;
+	int k=pow(a,3);  //the cubic of one value :
+	cout<<"the cubic of one value :"<<k<<endl;
+	int l=pow(b,2);  //the square of one value :
+	cout<<"the square of one value :"<<l<<endl;
+	float m=log10(a);  //the log value of one number :
+	cout<<"the log of one value :"<<m<<endl;
+	float n=log10(b);  //the log value of one numbers :
+	cout<<"the log of one value :"<<n<<endl;
+	double o=((a+b)*100)/20;  //the percentege value :
+	cout<<"the percentege of value :"<<o<<endl;
+	int p=pow(a,2);  //the square of one number number :
+	cout<<"the power of one value :"<<p<<endl;
+	int q=pow(b,3);  //the cubic value of one number :
+	cout<<"the power of one value :"<<q<<endl;
+	
+	
+	
+	
+	
+	
+	
+	
+}
