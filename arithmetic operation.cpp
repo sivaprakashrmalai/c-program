672b4166fedf92eaa8649712b0ad38f15e3f1aa4
sivaprakashrmalai <103348485+sivaprakashrmalai@users.noreply.github.com>
2023-02-01 14:04:30 +0530

    Create arithmetic operation.cpp

---
+++ arithmetic operation.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+	cout<<"ARITHMETIC OPERATOR:"<<endl;
+    int a, b;
+	cout<<"enter the number"<<endl;
+	cin>>a>>b;
+	cout<<"the addition value of two numbers"<<a+b<<endl;
+	cout<<"the subtraction value of two numbers"<<a-b<<endl;
+	cout<<"the multiplication value of two numbers"<<a*b<<endl;
+	cout<<"the division value of two numbers"<<a/b<<endl;
+	cout<<"the remainder value of two numbers"<<a%b<<endl;
+	return 0;
+	
+}
