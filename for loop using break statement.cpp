63adb0acb1e2730128a28ff9c3cf337c458d5ee5
sivaprakashrmalai <103348485+sivaprakashrmalai@users.noreply.github.com>
2023-02-01 14:19:23 +0530

    Create for loop using break statement.cpp

---
+++ for loop using break statement.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"enter the number"<<endl;
+	cin>>a;
+	for(a=1;a<=100;++a){
+		
+		if(a==51){
+			break;
+		}cout<<"print numbetr :"<<a<<endl;
+	}
+}
